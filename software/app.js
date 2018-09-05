0057d643d28fc626f44ffedcd31ba225c87c8ea3
SuddenDevelopment <Anthony.Aragues@gmail.com>
2018-09-04 21:18:53 -0400

    servo board test

--- software/app.js
+++ software/app.js
@@ -1,15 +1,37 @@
 var Cylon = require("cylon");
 
 Cylon.robot({
-  connections: {
-    raspi: { adaptor: 'raspi' }
-  },
+    connections: {
+        raspi: {
+            adaptor: 'raspi'
+        }
+    },
 
-  devices: {
-    led: { driver: 'led', pin: 11 }
-  },
+    devices: {
+        pca9685: {
+            driver: 'pca9685'
+        }
+    },
 
-  work: function(my) {
-    every((1).second(), my.led.toggle);
-  }
+    work: function(my) {
+
+        var min = 700;
+        var max = 1100;
+
+        // set the frequency to 60hz
+        my.pca9685.setPWMFreq(60);
+
+        // rotate to and hold the minium position
+        my.pca9685.setPWM(0, 0, min);
+
+        after((5).seconds(), function() {
+            // rotate to and hold the maxium position
+            my.pca9685.setPWM(0, 0, max);
+        });
+
+        after((10).seconds(), function() {
+            // reset and stop all outputs
+            my.pca9685.stop();
+        });
+    }
 }).start();
